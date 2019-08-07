fae3c070d2dda4243667cce2d152640dcf0616ae
r2 <r2@r2-2.local>
2019-08-07 00:49:49 -0400

    Testing sq apiw

--- res/main.js
+++ res/main.js
@@ -1,6 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-  var content = '<div id="form-container">' +
+
+
+  function checkout(item){
+  var container = document.getElementById('container');
+    console.log(item.childNodes[3].childNodes[3].dataset.price)
+  container.innerHTML = 
+    '<div id="form-container">' +
     '<div id="sq-ccbox">' +
     '<form id="nonce-form" novalidate action="process-payment" method="post">' +
     '<fieldset>' +
@@ -15,19 +21,31 @@ window.addEventListener('DOMContentLoaded', (event) => {
     '<div id="sq-postal-code"></div>' +
     '</div>' +
     '</fieldset>' +
-    '<button id="sq-creditcard" class="button-credit-card" onclick="onGetCardNonce(event)">Pay $1.00</button>' +
+    '<button id="sq-creditcard" class="button-credit-card" onclick="onGetCardNonce(event)">Pay ' + item.childNodes[3].childNodes[3].dataset.price + '</button>' +
     '<input type="hidden" id="card-nonce" name="nonce">' +
     '</form>' +
     '</div>' +
     '</div>';
+  }
+
+  let elementsArray = document.querySelectorAll(".item");
+  elementsArray.forEach(function(elem) {
+
+  elem.addEventListener('click', function(e){
+
+    checkout(e.currentTarget);
+  });
+  });
 
   // document.getElementsByTagName('body')[0].innerHTML = content;
   window.addEventListener('popstate', function(e) {
 
   });
 
-  //https://api.github.com/repos/testacc01/testrepo01/contents/test.txt
 
+// Get SHA from Github to replace file
+  //https://api.github.com/repos/testacc01/testrepo01/contents/test.txt
+/*
 
   async function getrepos(){
     const url = "https://api.github.com/repos/rflc/bots/contents/aux.py";
@@ -37,6 +55,5 @@ window.addEventListener('DOMContentLoaded', (event) => {
   }
 
   getrepos();
-
+*/
 });
-
---
+++ res/transact.js
@@ -0,0 +1,52 @@
+const applicationId = "REPLACE_WITH_APPLICATION_ID";
+
+function onGetCardNonce(event) {
+  event.preventDefault();
+  paymentForm.requestCardNonce();
+}
+
+const paymentForm = new SqPaymentForm({
+  applicationId: applicationId,
+  inputClass: 'sq-input',
+  inputStyles: [{
+    fontSize: '16px',
+        lineHeight: '24px',
+        padding: '16px',
+        placeholderColor: '#a0a0a0',
+        backgroundColor: 'transparent',
+  }],
+  cardNumber: {
+    elementId: 'sq-card-number',
+        placeholder: 'Card Number'
+  },
+  cvv: {
+
+        elementId: 'sq-cvv',
+        placeholder: 'CVV'
+  },
+  expirationDate: {
+
+        elementId: 'sq-expiration-date',
+        placeholder: 'MM/YY'
+  },
+  postalCode: {
+
+        elementId: 'sq-postal-code',
+        placeholder: 'Postal'
+  },
+  callbacks: {
+    cardNonceResponseReceived: function (errors, nonce, cardData) {
+
+          if (errors) {
+	    console.error('Encountered errors:');
+	          errors.forEach(function (error) {
+
+		                console.error('  ' + error.message);
+		  });
+	    alert('Encountered errors, check browser developer console for more details');
+	    return;
+	        }
+      alert(`The generated nonce is:\n${nonce}`);
+    }
+  }
+});
